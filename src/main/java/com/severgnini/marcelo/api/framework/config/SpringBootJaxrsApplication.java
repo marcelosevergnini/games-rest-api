dbf808bb32851f108fe9702b5303f7fcf72e08d7
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-25 18:07:49 +0000

    Enabled AutoConfiguraton

--- src/main/java/com/severgnini/marcelo/api/framework/config/SpringBootJaxrsApplication.java
+++ src/main/java/com/severgnini/marcelo/api/framework/config/SpringBootJaxrsApplication.java
@@ -1,11 +1,11 @@
 package com.severgnini.marcelo.api.framework.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.severgnini.marcelo.api.framework")
+@EnableAutoConfiguration
 public class SpringBootJaxrsApplication {
 
     public static void main(String... args){
