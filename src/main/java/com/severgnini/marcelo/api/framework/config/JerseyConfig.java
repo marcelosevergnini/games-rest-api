10739da459e186b5cb1660bf4bd684eab3456b39
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-25 18:07:28 +0000

    added @Component and ApplicationPath to the JerseyConfig

--- src/main/java/com/severgnini/marcelo/api/framework/config/JerseyConfig.java
+++ src/main/java/com/severgnini/marcelo/api/framework/config/JerseyConfig.java
@@ -1,10 +1,18 @@
 package com.severgnini.marcelo.api.framework.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
+@Component
+@ApplicationPath("/api")
 public class JerseyConfig  extends ResourceConfig{
 
+    @Autowired
     public JerseyConfig(){
-        packages("com.severgnini.marcelo.api.framework.service");
+        packages("com.severgnini.marcelo.api.framework.rest");
     }
+
 }
