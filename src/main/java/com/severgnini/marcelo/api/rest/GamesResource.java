aeca4ac8a31777d876a8af11e162bd2a0cbaa715
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-26 11:09:44 +0000

    moved pack to a right one

---
+++ src/main/java/com/severgnini/marcelo/api/rest/GamesResource.java
@@ -0,0 +1,24 @@
+package com.severgnini.marcelo.api.rest;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Component
+@Path("games")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class GamesResource {
+
+    @GET
+    @Path("up")
+    public Response getAll(){
+        return Response.ok().entity(new StringBuilder("Hello World, Working fine!!").toString()).build();
+    }
+
+}
