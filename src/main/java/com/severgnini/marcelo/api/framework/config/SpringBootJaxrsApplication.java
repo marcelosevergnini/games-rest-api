2917a3de8f71aa741bdbb80aec4667fd521541ac
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-26 23:40:30 +0000

    changed configuration to scan from package

--- src/main/java/com/severgnini/marcelo/api/framework/config/SpringBootJaxrsApplication.java
+++ src/main/java/com/severgnini/marcelo/api/framework/config/SpringBootJaxrsApplication.java
@@ -3,9 +3,12 @@ package com.severgnini.marcelo.api.framework.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages = {"com.severgnini.marcelo.api"})
 public class SpringBootJaxrsApplication {
 
     public static void main(String... args){
