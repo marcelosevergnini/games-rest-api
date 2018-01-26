1368009328d682a43974eb95c34e29c79817dd21
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-26 11:10:02 +0000

    Fixed Test

--- src/test/java/com/severgnini/marcelo/service/GamesResourcesTest.java
+++ src/test/java/com/severgnini/marcelo/service/GamesResourcesTest.java
@@ -3,13 +3,10 @@ package com.severgnini.marcelo.service;
 
 import com.severgnini.marcelo.api.framework.config.JerseyConfig;
 import com.severgnini.marcelo.api.framework.config.TestConfig;
-import com.severgnini.marcelo.api.framework.rest.GamesResource;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
@@ -21,7 +18,7 @@ public class GamesResourcesTest extends JerseyTest {
 
     public static final String ASSERT_SERVICE_RUNNING_BODY = "Hello World, Working fine!!";
 
-    private static final String RESOURCE_URL = "/api/games/";
+    private static final String RESOURCE_URL = "/games/";
 
     @Override
     protected Application configure() {
@@ -32,8 +29,9 @@ public class GamesResourcesTest extends JerseyTest {
     @Test
     public void serviceWorkingTest() {
 
-        Response response  = target(RESOURCE_URL + "all").request().get(Response.class);
-        assertTrue(ASSERT_SERVICE_RUNNING_BODY.equals(response.getEntity().toString()));
+        Response response  = target(RESOURCE_URL + "up").request().get(Response.class);
+        response.getLocation();
+        assertTrue("The Response Should be " + ASSERT_SERVICE_RUNNING_BODY, ASSERT_SERVICE_RUNNING_BODY.equals(response.readEntity(String.class)));
 
     }
 
