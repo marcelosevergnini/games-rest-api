35491cbf95fdbe91a6d7755fef477f3882dd0f48
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-26 23:39:44 +0000

    moved from package

--- src/main/java/com/severgnini/marcelo/api/framework/config/security/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.severgnini.marcelo.api.framework.config.security.enums;
-
-public enum Role {
-
-    USER,
-    ADMIN
-}
---
+++ src/main/java/com/severgnini/marcelo/api/framework/security/enums/Role.java
@@ -0,0 +1,7 @@
+package com.severgnini.marcelo.api.framework.security.enums;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
