0394685c8d1a6b8408d0c7b5ae75035ea8f0c4d9
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-30 14:25:03 +0000

    Implementing filter for the authorization based on this project: https://github.com/cassiomolin/jersey-jwt.git

--- src/main/java/com/severgnini/marcelo/api/framework/security/filter/AuthorizationFilter.java
+++ src/main/java/com/severgnini/marcelo/api/framework/security/filter/AuthorizationFilter.java
@@ -1,24 +1,18 @@
 package com.severgnini.marcelo.api.framework.security.filter;
 
-import com.severgnini.marcelo.api.framework.security.enums.Role;
-import com.severgnini.marcelo.api.framework.security.annotations.Secured;
-
 import javax.annotation.Priority;
+import javax.annotation.security.DenyAll;
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.AccessDeniedException;
 
-@Secured
 @Provider
 @Priority(Priorities.AUTHORIZATION)
 public class AuthorizationFilter  implements ContainerRequestFilter {
@@ -29,50 +23,16 @@ public class AuthorizationFilter  implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
 
-        // Get the resource class which matches with the requested URL
-        // Extract the roles declared by it
-        Class<?> resourceClass = resourceInfo.getResourceClass();
-        List<Role> classRoles = extractRoles(resourceClass);
+        Method method = resourceInfo.getResourceMethod();
 
-        // Get the resource method which matches with the requested URL
-        // Extract the roles declared by it
-        Method resourceMethod = resourceInfo.getResourceMethod();
-        List<Role> methodRoles = extractRoles(resourceMethod);
-
-        try {
-
-            // Check if the user is allowed to execute the method
-            // The method annotations override the class annotations
-            if (methodRoles.isEmpty()) {
-                checkPermissions(classRoles);
-            } else {
-                checkPermissions(methodRoles);
-            }
-
-        } catch (Exception e) {
-            requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+        if (method.isAnnotationPresent(DenyAll.class)) {
+            throw new AccessDeniedException("You don't have permissions!!");
         }
-    }
 
-    // Extract the roles from the annotated element
-    private List<Role> extractRoles(AnnotatedElement annotatedElement) {
-        if (annotatedElement == null) {
-            return new ArrayList<>();
-        } else {
-            Secured secured = annotatedElement.getAnnotation(Secured.class);
-            if (secured == null) {
-                return new ArrayList<Role>();
-            } else {
-                Role[] allowedRoles = secured.value();
-                return Arrays.asList(allowedRoles);
-            }
+        if (method.isAnnotationPresent(PermitAll.class)) {
+            return;
         }
-    }
 
-    private void checkPermissions(List<Role> allowedRoles) throws Exception {
-        // Check if the user contains one of the allowed roles
-        // Throw an Exception if the user has not permission to execute the method
     }
-
 
 }
