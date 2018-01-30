d2c548ef73d143d367fab1fe52b3520e8509be1f
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-30 14:27:41 +0000

    change register by package and added manually based on precedence

--- src/main/java/com/severgnini/marcelo/api/framework/config/JerseyConfig.java
+++ src/main/java/com/severgnini/marcelo/api/framework/config/JerseyConfig.java
@@ -1,18 +1,28 @@
 package com.severgnini.marcelo.api.framework.config;
 
+import com.severgnini.marcelo.api.framework.security.filter.AuthenticationFilter;
+import com.severgnini.marcelo.api.framework.security.filter.AuthorizationFilter;
+import com.severgnini.marcelo.api.rest.GamesResource;
+import com.severgnini.marcelo.api.rest.authentication.AuthenticationResource;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
 
 @Component
-@ApplicationPath("/api")
+@ApplicationPath("api")
 public class JerseyConfig  extends ResourceConfig{
 
-    @Autowired
     public JerseyConfig(){
-        packages("com.severgnini.marcelo.api.rest");
+        registerFilters();
+    }
+
+    private void registerFilters(){
+
+        register(AuthenticationResource.class);
+        register(AuthenticationFilter.class);
+        register(AuthorizationFilter.class);
+        register(GamesResource.class);
     }
 
 }
