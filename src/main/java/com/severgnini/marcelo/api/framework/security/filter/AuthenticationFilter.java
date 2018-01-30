0b443372b5e00b36a72c7b9a4178f5e186004e82
Marcelo Severgnini <marcelosevergnini@gmail.com>
2018-01-30 14:24:28 +0000

    Implementing filter for the authentication if the token is present based on this project: https://github.com/cassiomolin/jersey-jwt.git

---
+++ src/main/java/com/severgnini/marcelo/api/framework/security/filter/AuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.severgnini.marcelo.api.framework.security.filter;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String authenticationToken = authorizationHeader.substring(7);
+            // TODO implement token authentication
+            return;
+        }
+
+    }
+
+}
